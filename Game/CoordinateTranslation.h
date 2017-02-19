a386daf92bee5976cf991f6c16d612fc2bbdcb7e
sda97ghb <sda97g@gmail.com>
2017-02-19 19:22:49 +0500

    Added coordinate translation functions. Physical to sceen and sceen to physical.

---
+++ Game/CoordinateTranslation.h
@@ -0,0 +1,15 @@
+#ifndef COORDINATETRANSLATION_H
+#define COORDINATETRANSLATION_H
+
+#include "Box2D/Common/b2Math.h"
+#include "SFML/System/Vector2.hpp"
+
+static constexpr float SCREEN_PER_PHYSICAL = 50.0;
+
+sf::Vector2f physicalToScreen(const b2Vec2& pos);
+b2Vec2 screenToPhysical(const sf::Vector2f& pos);
+
+float physicalToScreen(float size);
+float screenToPhysical(float size);
+
+#endif // COORDINATETRANSLATION_H
