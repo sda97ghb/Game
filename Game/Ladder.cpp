89dc411fd416c8508053b1ad4143f86a0901f3e6
sda97ghb <sda97g@gmail.com>
2017-02-21 10:18:33 +0500

    New vision of ladder class

--- Game/Ladder.cpp
+++ Game/Ladder.cpp
@@ -3,16 +3,32 @@
 #include "Game/Player.h"
 #include "SFML/Window/Keyboard.hpp"
 
+Ladder::Ladder() :
+    _x(0.0f),
+    _y1(0.0f),
+    _y2(1.0f),
+    _width(1.0f)
+{
+}
+
 void Ladder::setCoordinates(float x, float y1, float y2)
 {
     _x = x;
     _y1 = y1;
     _y2 = y2;
+    setShape();
 }
 
 void Ladder::setWidth(float width)
 {
     _width = width;
+    setShape();
+}
+
+void Ladder::setTexture(const std::string& filename)
+{
+    _texture.loadFromFile(filename);
+    _texture.setRepeated(true);
 }
 
 void Ladder::testPlayerOnIt()
@@ -53,4 +69,48 @@ float Ladder::width() const
 float Ladder::height() const
 {
     return _y2 - _y1;
+}
+
+b2Vec2 Ladder::size() const
+{
+    return b2Vec2(width(), height());
+}
+
+b2PolygonShape&Ladder::shapeB2()
+{
+    return _shapeB2;
+}
+
+const b2PolygonShape&Ladder::shapeB2() const
+{
+    return _shapeB2;
+}
+
+sf::RectangleShape&Ladder::shapeSF()
+{
+    return _shapeSF;
+}
+
+const sf::RectangleShape&Ladder::shapeSF() const
+{
+    return _shapeSF;
+}
+
+sf::Texture&Ladder::texture()
+{
+    return _texture;
+}
+
+const sf::Texture&Ladder::texture() const
+{
+    return _texture;
+}
+
+void Ladder::setShape()
+{
+    b2Vec2 vertexes[4] = {b2Vec2(_x - _width / 2.0f, _y1),
+                          b2Vec2(_x + _width / 2.0f, _y1),
+                          b2Vec2(_x + _width / 2.0f, _y2),
+                          b2Vec2(_x - _width / 2.0f, _y2)};
+    _shapeB2.Set(vertexes, 4);
 }
--- Game/Ladder.h
+++ Game/Ladder.h
@@ -3,11 +3,18 @@
 
 #include "Game/Furniture.h"
 
+#include "Box2D/Collision/Shapes/b2PolygonShape.h"
+#include "SFML/Graphics/RectangleShape.hpp"
+#include "SFML/Graphics/Texture.hpp"
+
 class Ladder : public Furniture
 {
 public:
+    Ladder();
+
     void setCoordinates(float x, float y1, float y2);
     void setWidth(float width);
+    void setTexture(const std::string& filename);
 
     void testPlayerOnIt();
 
@@ -18,11 +25,26 @@ public:
     float width() const;
     float height() const;
 
+    b2Vec2 size() const;
+
+    b2PolygonShape& shapeB2();
+    const b2PolygonShape& shapeB2() const;
+    sf::RectangleShape& shapeSF();
+    const sf::RectangleShape& shapeSF() const;
+    sf::Texture& texture();
+    const sf::Texture& texture() const;
+
 private:
+    void setShape();
+
     float _x;
     float _y1;
     float _y2;
     float _width;
+
+    b2PolygonShape _shapeB2;
+    sf::RectangleShape _shapeSF;
+    sf::Texture _texture;
 };
 
 #endif // LADDER_H
