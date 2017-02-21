918d6073f0db44a9faebd70df7fd76a2605b3ee7
sda97ghb <sda97g@gmail.com>
2017-02-21 10:17:51 +0500

    Added platform class

---
+++ Game/Platform.cpp
@@ -0,0 +1,74 @@
+#include "Game/Platform.h"
+
+#include "Box2D/Collision/Shapes/b2PolygonShape.h"
+#include "Box2D/Dynamics/b2Fixture.h"
+
+void Platform::setTexture(const std::string& filename)
+{
+    _texture.loadFromFile(filename);
+    _texture.setRepeated(true);
+}
+
+b2PolygonShape& Platform::shapeB2()
+{
+    b2Fixture* fixture = _body->GetFixtureList();
+    if (fixture == nullptr)
+        throw NoShapeException();
+    b2Shape* shape = fixture->GetShape();
+    if (shape == nullptr)
+        throw NoShapeException();
+    return *static_cast<b2PolygonShape*>(shape);
+}
+
+const b2PolygonShape& Platform::shapeB2() const
+{
+    b2Fixture* fixture = _body->GetFixtureList();
+    if (fixture == nullptr)
+        throw NoShapeException();
+    b2Shape* shape = fixture->GetShape();
+    if (shape == nullptr)
+        throw NoShapeException();
+    return *static_cast<b2PolygonShape*>(shape);
+}
+
+void Platform::setBody(b2Body* body)
+{
+    _body = body;
+}
+
+void Platform::setShape(const b2PolygonShape& shape)
+{
+    if (_body == nullptr)
+        throw NoBodyException();
+    _body->CreateFixture(&shape, 0.0f);
+}
+
+b2Body& Platform::body()
+{
+    return *_body;
+}
+
+const b2Body&Platform::body() const
+{
+    return *_body;
+}
+
+sf::ConvexShape& Platform::shapeSF()
+{
+    return _shape;
+}
+
+const sf::ConvexShape&Platform::shapeSF() const
+{
+    return _shape;
+}
+
+sf::Texture& Platform::texture()
+{
+    return _texture;
+}
+
+const sf::Texture&Platform::texture() const
+{
+    return _texture;
+}
--- Game/Platform.h
+++ Game/Platform.h
@@ -1,9 +1,44 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+#include <stdexcept>
+#include <vector>
+
+#include <SFML/Graphics/ConvexShape.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+#include "Box2D/Common/b2Math.h"
+#include "Box2D/Dynamics/b2Body.h"
+#include "Box2D/Collision/Shapes/b2PolygonShape.h"
+
 class Platform
 {
-    ;
+public:
+    struct NoShapeException : public std::logic_error {
+        NoShapeException() : std::logic_error("there is no shape for the platform") {}
+    };
+
+    struct NoBodyException : public std::logic_error {
+        NoBodyException() : std::logic_error("there is no body for the shape") {}
+    };
+
+    void setBody(b2Body* body);
+    void setShape(const b2PolygonShape& shape);
+    void setTexture(const std::string& filename);
+
+    b2PolygonShape& shapeB2();
+    const b2PolygonShape& shapeB2() const;
+    b2Body& body();
+    const b2Body& body() const;
+    sf::ConvexShape& shapeSF();
+    const sf::ConvexShape& shapeSF() const;
+    sf::Texture& texture();
+    const sf::Texture& texture() const;
+
+private:
+    b2Body* _body;
+    sf::ConvexShape _shape;
+    sf::Texture _texture;
 };
 
 #endif // PLATFORM_H
