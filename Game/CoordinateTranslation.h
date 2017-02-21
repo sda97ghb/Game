e28dd4e6b664aec70e5a2ff13e350f3479ed6071
sda97ghb <sda97g@gmail.com>
2017-02-21 10:58:32 +0500

    // texture 16px == screen 32px == physical 1m

--- Game/CoordinateTranslation.h
+++ Game/CoordinateTranslation.h
@@ -5,7 +5,8 @@
 #include "SFML/System/Vector2.hpp"
 
 namespace translate {
-    static constexpr float SCREEN_PER_PHYSICAL = 30.0;
+    static constexpr float SCREEN_PER_PHYSICAL = 32.0;
+    // texture 16px == screen 32px == physical 1m
 
     // Point - Pos
     // Size - Size
