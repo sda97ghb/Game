4e2eb9122ac42d0b719ad3b880234370e5a03998
sda97ghb <sda97g@gmail.com>
2017-02-23 13:45:10 +0500

    Added bounds, testplayer and testarcher to testloader.

--- Test/TestWorldLoader.cpp
+++ Test/TestWorldLoader.cpp
@@ -13,8 +13,53 @@ void TestWorldLoader::load()
     {
         Platform& platform = world.createPlatform();
         b2PolygonShape shape;
-        b2Vec2 vertexes[4] = {b2Vec2(-20.0, -3.0),
-                              b2Vec2( 20.0, -3.0),
+        b2Vec2 vertexes[4] = {b2Vec2(-22.0, -6.0),
+                              b2Vec2( 22.0, -6.0),
+                              b2Vec2( 22.0, -5.5),
+                              b2Vec2(-22.0, -5.5)};
+        shape.Set(vertexes, 4);
+        platform.setShape(shape);
+        platform.setTexture("C:/Projects/Game/Textures/stonebricks.png");
+    }
+    {
+        Platform& platform = world.createPlatform();
+        b2PolygonShape shape;
+        b2Vec2 vertexes[4] = {b2Vec2(-22.0, 20.0),
+                              b2Vec2( 22.0, 20.0),
+                              b2Vec2( 22.0, 20.5),
+                              b2Vec2(-22.0, 20.5)};
+        shape.Set(vertexes, 4);
+        platform.setShape(shape);
+        platform.setTexture("C:/Projects/Game/Textures/stonebricks.png");
+    }
+    {
+        Platform& platform = world.createPlatform();
+        b2PolygonShape shape;
+        b2Vec2 vertexes[4] = {b2Vec2(-22.5, -6.0),
+                              b2Vec2(-22.0, -6.0),
+                              b2Vec2(-22.0, 20.5),
+                              b2Vec2(-22.5, 20.5)};
+        shape.Set(vertexes, 4);
+        platform.setShape(shape);
+        platform.setTexture("C:/Projects/Game/Textures/stonebricks.png");
+    }
+    {
+        Platform& platform = world.createPlatform();
+        b2PolygonShape shape;
+        b2Vec2 vertexes[4] = {b2Vec2(22.0, -6.0),
+                              b2Vec2(22.5, -6.0),
+                              b2Vec2(22.5, 20.0),
+                              b2Vec2(22.0, 20.0)};
+        shape.Set(vertexes, 4);
+        platform.setShape(shape);
+        platform.setTexture("C:/Projects/Game/Textures/stonebricks.png");
+    }
+
+    {
+        Platform& platform = world.createPlatform();
+        b2PolygonShape shape;
+        b2Vec2 vertexes[4] = {b2Vec2(-20.0, -30.0),
+                              b2Vec2( 20.0, -30.0),
                               b2Vec2( 20.0,  0.0),
                               b2Vec2(-20.0,  0.0)};
         shape.Set(vertexes, 4);
@@ -90,13 +135,20 @@ void TestWorldLoader::load()
 
     {
         Ladder& ladder = world.createLadder();
-        ladder.setCoordinates(-5.5, 0.0, 7.5);
-        ladder.setWidth(1.0);
+        ladder.setCoordinates(-5.5f, 0.0f, 7.5f);
+        ladder.setWidth(1.0f);
         ladder.setTexture("C:/Projects/Game/Textures/ladder.png");
     }
 
-//    Enemy enemy1;
-//    enemy1.setTexture("");
-//    enemy1.setPosition(0.0, 0.0);
-//    world.addEnemy(enemy1);
+    {
+        TestPlayer& player = world.player();
+        player.setPosition(0.0f, 1.75f / 2.0f);
+        player.setTexture("C:/Projects/Game/Textures/player.png");
+    }
+
+    {
+        TestArcher& archer = world.createArcher();
+        archer.setPosition(3.0f, 10.0f);
+        archer.setTexture("C:/Projects/Game/Textures/archer.png");
+    }
 }
