9ee2c23fb8394dd5f4c676175c2e9db65c17adae
sda97ghb <sda97g@gmail.com>
2017-02-25 07:57:45 +0500

    Added constructEntity method in World class

--- Game/World.cpp
+++ Game/World.cpp
@@ -17,111 +17,10 @@ World::~World()
     delete _world;
 }
 
-void World::createPlayer()
-{
-    b2BodyDef entityBodyDef;
-    entityBodyDef.type = b2_dynamicBody;
-    entityBodyDef.fixedRotation = true;
-    entityBodyDef.position.Set(0.0f, 0.0f);
-    player().setBody(_world->CreateBody(&entityBodyDef));
-    {
-        b2PolygonShape playerShape;
-        playerShape.SetAsBox(0.75f / 2.0f, 1.75f / 2.0f);
-        b2FixtureDef playerFixtureDef;
-        playerFixtureDef.shape = &playerShape;
-        playerFixtureDef.density = 1.0f;
-        playerFixtureDef.friction = 20.0f;
-        playerFixtureDef.restitution = 0.0f;
-        player().body().CreateFixture(&playerFixtureDef);
-    }
-    {
-        b2PolygonShape sensorShape;
-        sensorShape.SetAsBox(0.3f, 0.3f, b2Vec2(0.0f, -1.75f / 2.0f), 0.0f);
-        b2FixtureDef sensorFixtureDef;
-        sensorFixtureDef.shape = &sensorShape;
-        sensorFixtureDef.isSensor = true;
-        b2Fixture* fixture = player().body().CreateFixture(&sensorFixtureDef);
-        fixture->SetUserData((void*)SensorContactListener::GROUND_SENSOR_TYPE);
-    }
-}
-
 World& World::instance()
 {
     static World instance;
     return instance;
-}
-
-Player& World::player()
-{
-    return Player::instance();
-}
-
-Archer& World::createArcher()
-{
-    _archers.emplace_back();
-    Archer& archer = _archers.back();
-
-    b2BodyDef archerBodyDef;
-    archerBodyDef.type = b2_dynamicBody;
-    archerBodyDef.fixedRotation = true;
-    archerBodyDef.position.Set(0.0f, 0.0f);
-    archer.setBody(_world->CreateBody(&archerBodyDef));
-    {
-        b2PolygonShape archerShape;
-        archerShape.SetAsBox(0.75f / 2.0f, 1.75f / 2.0f);
-        b2FixtureDef archerFixtureDef;
-        archerFixtureDef.shape = &archerShape;
-        archerFixtureDef.density = 1.0f;
-        archerFixtureDef.friction = 20.0f;
-        archerFixtureDef.restitution = 0.0f;
-        archer.body().CreateFixture(&archerFixtureDef);
-    }
-    {
-        b2PolygonShape sensorShape;
-        sensorShape.SetAsBox(0.3f, 0.3f, b2Vec2(0.0f, -1.75f / 2.0f), 0.0f);
-        b2FixtureDef sensorFixtureDef;
-        sensorFixtureDef.shape = &sensorShape;
-        sensorFixtureDef.isSensor = true;
-        b2Fixture* fixture = archer.body().CreateFixture(&sensorFixtureDef);
-        fixture->SetUserData((void*)SensorContactListener::GROUND_SENSOR_TYPE);
-    }
-
-    return archer;
-}
-
-const std::list<Archer>& World::archers() const
-{
-    return _archers;
-}
-
-const std::list<Ladder>& World::ladders() const
-{
-    return _ladders;
-}
-
-void World::update()
-{
-    float32 timeStep = 1.0f / 60.0f;
-    int32 velocityIterations = 8;
-    int32 positionIterations = 3;
-
-    _world->Step(timeStep, velocityIterations, positionIterations);
-
-    for (Ladder& ladder : _ladders)
-        ladder.testPlayerOnIt();
-}
-
-const std::list<Platform>& World::platforms() const
-{
-    return _platforms;
-}
-
-Ladder& World::createLadder()
-{
-    _ladders.emplace_back();
-    Ladder& ladder = _ladders.back();
-
-    return ladder;
 }
 
 Platform& World::createPlatform()
@@ -134,4 +33,116 @@ Platform& World::createPlatform()
     platform.setBody(_world->CreateBody(&bodyDef));
 
     return platform;
+}
+
+Ladder& World::createLadder()
+{
+    _ladders.emplace_back();
+    Ladder& ladder = _ladders.back();
+
+    return ladder;
+}
+
+void World::constructEntity(Entity& entity) const
+{
+    const float width = entity.width();
+    const float height = entity.height();
+    {
+        b2PolygonShape entityShape;
+        entityShape.SetAsBox(width / 2.0f, height / 2.0f);
+        b2FixtureDef entityFixtureDef;
+        entityFixtureDef.shape = &entityShape;
+        entityFixtureDef.density = 1.0f;
+        entityFixtureDef.friction = 20.0f;
+        entityFixtureDef.restitution = 0.0f;
+        entity.body().CreateFixture(&entityFixtureDef);
+    }
+    {
+        b2PolygonShape sensorShape;
+        sensorShape.SetAsBox(width / 2.0f * 0.9, 0.1,
+                             b2Vec2(0.0f, -height / 2.0f), 0.0f);
+        b2FixtureDef sensorFixtureDef;
+        sensorFixtureDef.shape = &sensorShape;
+        sensorFixtureDef.isSensor = true;
+        b2Fixture* fixture = entity.body().CreateFixture(&sensorFixtureDef);
+        fixture->SetUserData((void*)SensorContactListener::GROUND_SENSOR_TYPE);
+    }
+    {
+        b2PolygonShape sensorShape;
+        sensorShape.SetAsBox(0.1f, height / 2.0 * 0.9,
+                             b2Vec2(-width / 2.0f, 0.0), 0.0f);
+        b2FixtureDef sensorFixtureDef;
+        sensorFixtureDef.shape = &sensorShape;
+        sensorFixtureDef.isSensor = true;
+        b2Fixture* fixture = entity.body().CreateFixture(&sensorFixtureDef);
+        fixture->SetUserData((void*)SensorContactListener::LEFT_SENSOR_TYPE);
+    }
+    {
+        b2PolygonShape sensorShape;
+        sensorShape.SetAsBox(0.1f, height / 2.0 * 0.9,
+                             b2Vec2(width / 2.0f, 0.0), 0.0f);
+        b2FixtureDef sensorFixtureDef;
+        sensorFixtureDef.shape = &sensorShape;
+        sensorFixtureDef.isSensor = true;
+        b2Fixture* fixture = entity.body().CreateFixture(&sensorFixtureDef);
+        fixture->SetUserData((void*)SensorContactListener::RIGHT_SENSOR_TYPE);
+    }
+}
+
+void World::createPlayer()
+{
+    b2BodyDef entityBodyDef;
+    entityBodyDef.type = b2_dynamicBody;
+    entityBodyDef.fixedRotation = true;
+    entityBodyDef.position.Set(0.0f, 0.0f);
+    player().setBody(_world->CreateBody(&entityBodyDef));
+
+    constructEntity(player());
+}
+
+Archer& World::createArcher()
+{
+    _archers.emplace_back();
+    Archer& archer = _archers.back();
+
+    b2BodyDef archerBodyDef;
+    archerBodyDef.type = b2_dynamicBody;
+    archerBodyDef.fixedRotation = true;
+    archerBodyDef.position.Set(0.0f, 0.0f);
+    archer.setBody(_world->CreateBody(&archerBodyDef));
+    constructEntity(archer);
+
+    return archer;
+}
+
+Player& World::player()
+{
+    return Player::instance();
+}
+
+const std::list<Platform>& World::platforms() const
+{
+    return _platforms;
+}
+
+const std::list<Ladder>& World::ladders() const
+{
+    return _ladders;
+}
+
+const std::list<Archer>& World::archers() const
+{
+    return _archers;
+}
+
+void World::update()
+{
+    float32 timeStep = 1.0f / 60.0f;
+    int32 velocityIterations = 8;
+    int32 positionIterations = 3;
+
+    _world->Step(timeStep, velocityIterations, positionIterations);
+
+    for (Ladder& ladder : _ladders)
+        ladder.testPlayerOnIt();
 }
