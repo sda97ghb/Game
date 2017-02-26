3846f118aa7bce1e5778e064c12eba9134b702e1
VolkovAlexanderVA <Aleks_miass@mail.ru>
2017-02-26 19:44:43 +0500

    Убраны коментарии.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "Test/Test.h"
-//#include "Test/PhysicsPainter.h"
 
 #include <iostream>
 
@@ -7,7 +6,6 @@ int main(int argc, char* argv[])
 {
     (void)argc;
     (void)argv;
-//    PhysicsPainter::instance().run(argc, argv);
     test::main(argc, argv);
     std::cout << "Done." << std::endl;
     return 0;
