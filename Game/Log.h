628884890cde0d49d45773d3fcf240dc1201fb50
sda97ghb <sda97g@gmail.com>
2017-02-28 16:01:02 +0500

    Added Log class documentation.

--- Game/Log.h
+++ Game/Log.h
@@ -1,27 +1,39 @@
+/// \file
+/// \brief Содержит класс лога (чата).
+
 #ifndef LOG_H
 #define LOG_H
 
 #include <list>
 #include <string>
 
+/// \brief Класс лога (чата).
+/// \note Вы можете обратиться к логу из любого места вызвав Log::instance()
 class Log
 {
 public:
+    /// \brief Возвращает ссылку на объект лога.
     static Log& instance();
 
+    /// \brief Добавляет новое сообщение.
     void push(const std::string& line);
 
+    /// \brief Возвращает константный список всех сообщений.
     const std::list<std::string>& lines() const;
 
+    /// \brief Возвращает максимальное количество сообщений, которое может
+    /// находится одновременно в логе.
     int logSize() const;
+
+    /// \brief Устанавливает максимальное количество одновременных сообщений.
     void setLogSize(int logSize);
 
 private:
     Log();
 
-    std::list<std::string> _messages;
+    std::list<std::string> _messages; ///< сообщения
 
-    int _logSize;
+    uint32_t _logSize; ///< максимально возможное число одновременных сообщений
 };
 
 #endif // LOG_H
