443d44cd7b4864d22b94ecd117a1b57ab57e044b
sda97ghb <sda97g@gmail.com>
2017-02-28 15:24:21 +0500

    Added documentation for ImageScaler class.

--- Game/ImageScaler.h
+++ Game/ImageScaler.h
@@ -1,11 +1,18 @@
+/// \file
+/// \brief Содержит класс масштабирования sf::Image изображений.
+
 #ifndef IMAGESCALER_H
 #define IMAGESCALER_H
 
 #include "SFML/Graphics/Image.hpp"
 
+/// \brief Класс масштабирования изображения sf::Image.
 class ImageScaler
 {
 public:
+    /// \brief Масштабирует изображение, увеличивая в два раза.
+    /// \note Не сглаживает при масштабировании.
+    /// \return Увеличенную копию изображения.
     static sf::Image scale(const sf::Image& original);
 };
 
