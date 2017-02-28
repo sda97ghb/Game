2ca16581ffe925047970fd1b1edb54533be3c940
sda97ghb <sda97g@gmail.com>
2017-02-28 16:02:32 +0500

    Added documentation for MouseController.

--- Game/MouseController.h
+++ Game/MouseController.h
@@ -1,9 +1,14 @@
+/// \file
+/// \brief Содержит класс обработки мыши.
+
 #ifndef MOUSECONTROLLER_H
 #define MOUSECONTROLLER_H
 
+/// \brief Класс обработки мыши.
 class MouseController
 {
 public:
+    /// \brief Обработка текущего состояния мыши.
     static void processMouse();
 };
 
