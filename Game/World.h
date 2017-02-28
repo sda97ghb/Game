ec10323c14ca1d2fe858aa412b82496890357f57
sda97ghb <sda97g@gmail.com>
2017-02-28 15:04:11 +0500

    Added documentation for World class.

--- Game/World.h
+++ Game/World.h
@@ -1,3 +1,6 @@
+/// \file
+/// \brief Содержит класс мира.
+
 #ifndef WORLD_H
 #define WORLD_H
 
@@ -18,26 +21,42 @@
 #include "Game/Swordman.h"
 #include "Game/Water.h"
 
+/// \brief Класс мира.
+/// \details Хранит физический мир, платформы, объекты взаимодействия,
+/// существ. Предоставляет фабричные методы добавления новых объектов.
+/// Отвечает за обновление мира.
+/// \note Это класс-одиночка.
 class World
 {
 public:
     ~World();
+    /// \brief Ссылка на объект мира. (не физического, а вообще)
     static World& instance();
 
+    /// \brief Метод добавления платформы.
     Platform& createPlatform();
 
+    /// \brief Метод добавления лестницы.
     Ladder& createLadder();
 
+    /// \brief Метод добавления лучника.
     Archer& createArcher();
 
+    /// \brief Возвращает ссылку на игрока.
+    /// \note Вы можете использовать Player::instance() вместо этого
+    /// метода.
     Player& player();
 
+    /// \brief Возвращает константный список всех платформ.
     const std::list<Platform>& platforms() const;
 
+    /// \brief Возвращает константный список всех лестниц.
     const std::list<Ladder>& ladders() const;
 
+    /// \brief Возвращает константный список всех лучников.
     const std::list<Archer>& archers() const;
 
+    /// \brief Обновляет мир.
     void update();
 
 private:
@@ -46,16 +65,19 @@ private:
     World(const World&) = delete;
     void operator= (const World&) = delete;
 
+    /// \brief Создает физическое тело для существ.
     b2Body* createBodyForEntity();
 
+    /// \brief Создает физическое тело для игрока.
     void createPlayer();
 
-    std::list<Ladder> _ladders;
-    std::list<Platform> _platforms;
+    std::list<Platform> _platforms; ///< список всех платформ
 
-    std::list<Archer> _archers;
+    std::list<Ladder> _ladders; ///< список всех лестниц
 
-    b2World* _world;
+    std::list<Archer> _archers; ///< список всех лучников
+
+    b2World* _world; ///< физический мир
 };
 
 #endif // WORLD_H
