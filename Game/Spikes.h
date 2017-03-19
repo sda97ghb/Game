03ebca3054625f346e869ba72389879b06bea516
VolkovAlexanderVA <Aleks_miass@mail.ru>
2017-03-19 15:25:00 +0500

    Исправлена табуляция.

--- Game/Spikes.h
+++ Game/Spikes.h
@@ -16,10 +16,10 @@ public:
     void setBegin(float x, float y);
     void setEnd(float x, float y);
 
-	Spikes();
+    Spikes();
 
-	void setCoordinates(float x1, float y1, float x2, float y2);
-	void setHeight(float height);
+    void setCoordinates(float x1, float y1, float x2, float y2);
+    void setHeight(float height);
     void testPlayerOnIt();
 
     float x1() const;
@@ -27,31 +27,30 @@ public:
     float y1() const;
     float y2() const;
 
-	float height() const;
-	float lenght() const;
-	float sign(float sign) const;
-	float angle() const;
+    float height() const;
+    float lenght() const;
+    float sign(float sign) const;
+    float angle() const;
 
-	/// \brief Физическая фигура шипов.
-	b2PolygonShape& shapeB2();
-	const b2PolygonShape& shapeB2() const;
+    /// \brief Физическая фигура шипов.
+    b2PolygonShape& shapeB2();
+    const b2PolygonShape& shapeB2() const;
 
-	/// \brief Графическая фигура шипов.
-	sf::RectangleShape& shapeSF();
-	const sf::RectangleShape& shapeSF() const;
+    /// \brief Графическая фигура шипов.
+    sf::RectangleShape& shapeSF();
+    const sf::RectangleShape& shapeSF() const;
 
 private:
-
-	void setShape();
+    void setShape();
 
     float _x1;
     float _x2;
     float _y1;
     float _y2;
-	float _height;
+    float _height;
 
-	b2PolygonShape _shapeB2;
-	sf::RectangleShape _shapeSF;
+    b2PolygonShape _shapeB2;
+    sf::RectangleShape _shapeSF;
 };
 
 #endif // SPIKES_H
