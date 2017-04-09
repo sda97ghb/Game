78e8d58353790c826c0aa23d73b86c6945096d7c
sda97ghb <sda97g@gmail.com>
2017-04-10 00:32:56 +0500

    Added gravity value as global constant.

--- Arena/GlobalConstants.h
+++ Arena/GlobalConstants.h
@@ -3,6 +3,15 @@
 
 #include <cstdint>
 
+#include "Box2D/Common/b2Math.h"
+
+// -------- Physics constants -------- //
+
+static const float g = 9.8f;
+static const b2Vec2 GRAVITY_VECTOR = b2Vec2(0.0f, -::g);
+
+// -------- Graphic constants -------- //
+
 static const uint32_t SCREEN_RESOLUTION_X = 1280;
 static const uint32_t SCREEN_RESOLUTION_Y = 720;
 
