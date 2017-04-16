2da275120b666f9a93bc51b87a093c8d8a09f98b
sda97ghb <sda97g@gmail.com>
2017-04-17 00:39:45 +0500

    Добавлен сенсор столкновения с существом.

---
+++ Arena/Sensors/EnityCollisionSensor.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+#include "Box2D/Collision/Shapes/b2PolygonShape.h"
+#include "Box2D/Dynamics/b2Fixture.h"
+
+#include "Arena/IdDispenser.h"
+#include "Arena/ObjectCounter.h"
+
+#include "Arena/Sensors/EnityCollisionSensor.h"
+
+EnityCollisionSensor::EnityCollisionSensor() :
+    _x(0.0f),
+    _y(0.0f),
+    _width(0.0f),
+    _height(0.0f),
+    _type(IdDispenser::getNewId()),
+    _body(nullptr),
+    _onCollisionCallback([](Entity*){})
+{
+    ObjectCounter<EnityCollisionSensor>::addObject(this);
+}
+
+EnityCollisionSensor::~EnityCollisionSensor()
+{
+    ObjectCounter<EnityCollisionSensor>::removeObject(this);
+}
+
+void EnityCollisionSensor::setPosition(float x, float y)
+{
+    _x = x;
+    _y = y;
+}
+
+void EnityCollisionSensor::setSize(float width, float height)
+{
+    _width = width;
+    _height = height;
+}
+
+void EnityCollisionSensor::setType(int type)
+{
+    _type = type;
+}
+
+void EnityCollisionSensor::hangOnBody(b2Body* body)
+{
+    _body = body;
+
+    b2PolygonShape sensorShape;
+    sensorShape.SetAsBox(_width, _height, b2Vec2(_x, _y), 0.0f);
+
+    b2FixtureDef sensorFixtureDef;
+    sensorFixtureDef.shape = &sensorShape;
+    sensorFixtureDef.isSensor = true;
+
+    b2Fixture* fixture = _body->CreateFixture(&sensorFixtureDef);
+    fixture->SetUserData((void*)_type);
+}
+
+void EnityCollisionSensor::hit(b2Body* body)
+{
+    for (Entity* entity : ObjectCounter<Entity>::objects())
+        if (entity->body() == body)
+            _onCollisionCallback(entity);
+}
+
+void EnityCollisionSensor::setOnCollisionCallback(const EnityCollisionSensor::OnCollisionCallback& callback)
+{
+    _onCollisionCallback = callback;
+}
+
+int EnityCollisionSensor::type() const
+{
+    return _type;
+}
+
+b2Body* EnityCollisionSensor::body() const
+{
+    return _body;
+}
---
+++ Arena/Sensors/EnityCollisionSensor.h
@@ -0,0 +1,47 @@
+#ifndef ENITYCOLLISIONSENSOR_H
+#define ENITYCOLLISIONSENSOR_H
+
+#include <functional>
+
+#include "Box2D/Dynamics/b2Body.h"
+
+#include "Arena/Entity/Entity.h"
+
+class EnityCollisionSensor
+{
+public:
+    using OnCollisionCallback = std::function<void(Entity*)>;
+
+    EnityCollisionSensor();
+    ~EnityCollisionSensor();
+
+    void setPosition(float x, float y);
+
+    void setSize(float width, float height);
+
+    void setType(int type);
+
+    void hangOnBody(b2Body* body);
+
+    void hit(b2Body* body);
+
+    void setOnCollisionCallback(const OnCollisionCallback& callback);
+
+    int type() const;
+
+    b2Body* body() const;
+
+private:
+    float _x;
+    float _y;
+    float _width;
+    float _height;
+
+    int _type;
+
+    b2Body* _body;
+
+    OnCollisionCallback _onCollisionCallback;
+};
+
+#endif // ENITYCOLLISIONSENSOR_H
