3ff93be3138dd05dd5f14f3d136b7281a9f85d2c
sda97ghb <sda97g@gmail.com>
2017-05-07 16:19:15 +0500

    Новый фон

--- Arena/PaintingWindow.cpp
+++ Arena/PaintingWindow.cpp
@@ -77,7 +77,7 @@ void PaintingWindow::drawWorld()
 {
 //    b2Vec2 playerPos = World::instance().player()->body()->GetPosition();
 //    _worldView.setCenter(playerPos.x, playerPos.y + 5.0f);
-    _worldView.setCenter(0.0f, 3.5f);
+    _worldView.setCenter(0.0f, 4.5f);
 //    _worldView.setCenter(0.0f, SCREEN_RESOLUTION_Y / PIXELART_SCALE_FACTOR / PIXELS_PER_METER / 2.0f -
 //                         6.0f / PIXELS_PER_METER);
     setView(_worldView);
